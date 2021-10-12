4951cf11fd5e8ec4625882f59a1c929cf5a731ff
Zaher <ilbihz@gmail.com>
2021-10-12 15:30:56 -0400

    pseudocode for ex41

---
+++ Ex41/exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,51 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution41 {
+
+
+    /*
+    Create a program that reads in the following list of names from a file called
+     `exercise41_input.txt` and sorts the list alphabetically:
+
+    Ling, Mai
+    Johnson, Jim
+    Zarnecki, Sabrina
+    Jones, Chris
+    Jones, Aaron
+    Swift, Geoffrey
+    Xiong, Fong
+
+    Then print the sorted list to a file called `exercise41_output.txt`
+    that looks like the following example output.
+     */
+
+    //Method that attains all the names in the input file and returns the list
+    //filled with the names
+    public List<String> getInputFromFile(){
+
+
+    }
+    //method that takes a list of names and organizes them by alphabetical order
+    public List<String> organizeNames(List<String> names){
+
+    }
+
+
+    //main method
+    public static void main(String[] args){
+
+
+
+
+    }
+
+}
