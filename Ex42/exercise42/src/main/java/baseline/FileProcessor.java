3861bb4348a51c74f36d28ca0bd4811ba8cadfa5
Zaher <ilbihz@gmail.com>
2021-10-13 14:09:05 -0400

    pseudocode for ex42

---
+++ Ex42/exercise42/src/main/java/baseline/FileProcessor.java
@@ -0,0 +1,28 @@
+package baseline;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileProcessor {
+
+    //method that reads in the file line by line to later separate into necessary values
+    public List<String> getInputFromFile(){
+
+        File input = null;
+        List<String> info = new ArrayList<>();
+
+    }
+    //method that takes a list of CSV and separates the information compactly
+    public List<String> processInformation(List<String> info){
+
+
+    }
+
+    //method that displays the information in a nicely formatted table
+    public void displayInformation(List<String> processedInfo){
+
+
+    }
+
+}
---
+++ Ex42/exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,39 @@
+package baseline;
+
+public class Solution42 {
+
+    /*
+    Construct a program that reads in the following data file
+     (you will need to create this data file yourself; name it `exercise42_input.txt`):
+
+    Ling,Mai,55900
+    Johnson,Jim,56500
+    Jones,Aaron,46000
+    Jones,Chris,34500
+    Swift,Geoffrey,14200
+    Xiong,Fong,65000
+    Zarnecki,Sabrina,51500
+
+    Process the records and display the results formatted as a table, evenly spaced,
+    as shown in the example output.
+
+    Last      First     Salary
+    --------------------------
+    Ling      Mai       55900
+    Johnson   Jim       56500
+    Jones     Aaron     46000
+    Jones     Chris     34500
+    Swift     Geoffrey  14200
+    Xiong     Fong      65000
+    Zarnecki  Sabrina   51500
+    */
+
+    //main method
+    public static void main(String[] args){
+
+
+
+
+    }
+
+}
