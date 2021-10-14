1d35b7680eff1ed5bef383005e144f09e35fbb6b
Zaher <ilbihz@gmail.com>
2021-10-13 20:30:04 -0400

    pseudocode for ex43

---
+++ Ex43/exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,50 @@
+package baseline;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Solution43 {
+
+    /*
+    Create a program that generates a website skeleton with the following specifications:
+
+    Prompt for the name of the site.
+    Prompt for the author of the site.
+    Ask if the user wants a folder for JavaScript files.
+    Ask if the user wants a folder for CSS files.
+    Generate an index.html file that contains the name of the site
+        inside the <title> tag and the author in a <meta> tag.
+     */
+
+
+    //function that generates a website skeleton based on a few specifications
+    public void generateWebsiteSkeleton(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    //scanner objet for input
+    public static final Scanner scan = new Scanner(System.in);
+
+    //main method
+    public static void main(String[] args){
+
+
+
+    }
+
+}
