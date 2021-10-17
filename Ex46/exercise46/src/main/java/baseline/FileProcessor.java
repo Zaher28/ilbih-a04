1f2edcce71e12a5c7f13cb7884759def7f5ca5fd
Zaher <ilbihz@gmail.com>
2021-10-17 15:03:24 -0400

    bare pseudocode for ex46

---
+++ Ex46/exercise46/src/main/java/baseline/FileProcessor.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class FileProcessor {
+
+    //method that processes an input file into a Map with the frequency of each word
+    //returns the Map
+    public Map<String,Integer> processFile(){
+
+
+
+
+
+    }
+    //method that takes in a map of values and their frequencies and displays a histogram
+    public void printOutFrequencies(Map<String,Integer> mp){
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Ex46/exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+public class Solution46 {
+
+
+    /*
+    Create a program that reads in a file named `exercise46_input.txt` and counts the frequency of words in the file. Then construct a histogram displaying the words and the frequency, and display the histogram to the screen.
+    Example Output
+
+    Given the text file `exercise46_input.txt` with this content
+
+    badger badger badger
+    badger mushroom
+    mushroom snake badger badger
+    badger
+
+    the program would produce the following output:
+
+    badger:   *******
+    mushroom: **
+    snake:    *
+     */
+
+    //main method
+    public static void main(String[] args){
+
+        FileProcessor fp = new FileProcessor();
+
+    }
+
+}
