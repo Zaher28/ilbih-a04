0cc3fcd3037e9ce46fa31ad17359837924d967dd
Zaher <ilbihz@gmail.com>
2021-10-16 20:41:33 -0400

    finished code for ex43

--- Ex43/exercise43/src/main/java/baseline/Solution43.java
+++ Ex43/exercise43/src/main/java/baseline/Solution43.java
@@ -1,6 +1,8 @@
 package baseline;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Solution43 {
@@ -20,20 +22,62 @@ public class Solution43 {
     //function that generates a website skeleton based on a few specifications
     public void generateWebsiteSkeleton(){
 
+        System.out.print("Site name: ");
+        String siteName = scan.nextLine();
+
+        System.out.print("Author: ");
+        String author = scan.nextLine();
+
+        System.out.print("Do you want a folder for JavaScript? ");
+        String javaScript = scan.nextLine();
+
+        System.out.print("Do you want a folder for CSS? ");
+        String css = scan.nextLine();
+
+        String web = "./website/";
+
+        try{
+            //creates directories for the html file
+            if(new File(web +siteName).mkdirs()){
+
+                System.out.println("Created " +web + siteName);
+            }
+            //creates the actual html file
+            File website = new File(web +siteName + "/index.html");
+            if(website.createNewFile()){
+
+                System.out.println("Created "+ web + siteName + "/" + website.getName());
+                FileWriter wr = new FileWriter(website);
+                wr.write("<html><title>"+ siteName +"</title>\n");
+                wr.write("<meta>"+ author + "</meta> </html>");
+                wr.close();
+            }
+
+            if((javaScript.equals("y") || javaScript.equals("Y")) && new File(web +siteName + "/js").mkdir()){
+
+
+                    System.out.println("Created " + web + siteName + "/js/");
+
+
+
+            }
+
+            if((css.equals("y") || css.equals("Y")) && new File(web +siteName + "/css").mkdir()){
+
+
+                System.out.println("Created " + web + siteName + "/css/");
+
+
+
+            }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+        }catch(Exception e){
+            System.out.println("Sorry! An error happened.");
+        }
 
     }
 
@@ -43,6 +87,8 @@ public class Solution43 {
     //main method
     public static void main(String[] args){
 
+        Solution43 sol = new Solution43();
+        sol.generateWebsiteSkeleton();
 
 
     }
