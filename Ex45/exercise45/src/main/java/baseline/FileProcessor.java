89017746c0de0f9453490aca112d199c0e6e9b94
Zaher <ilbihz@gmail.com>
2021-10-17 13:44:29 -0400

    bare pseudocode for ex45

---
+++ Ex45/exercise45/src/main/java/baseline/FileProcessor.java
@@ -0,0 +1,27 @@
+package baseline;
+
+
+
+public class FileProcessor {
+
+    //parameter to keep all words of the text file
+    private String[] words;
+
+    //method that goes through the File and replaces each instance of 'utilize' with 'use'
+    public void processFile(){
+
+
+
+
+
+
+    }
+
+
+    //method that takes in the name of the output file and writes to that file
+    public void writeToFile(String fileName){
+
+
+    }
+
+}
---
+++ Ex45/exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,26 @@
+package baseline;
+
+public class Solution45 {
+
+
+    /*
+    There will be times when you'll need to read in one file, modify it,
+    and then write a modified version of that file to a new file.
+
+    Given an input file named `exercise45_input.txt`,
+    read the file and look for all occurrences of the word utilize.
+    Replace each occurrence with use. Write the modified file to a new file.
+    */
+
+
+    //main method
+    public static void main(String[] args){
+
+        FileProcessor sol = new FileProcessor();
+
+
+
+    }
+
+
+}
