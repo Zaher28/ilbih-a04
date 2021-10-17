ef286b72d811ea4546d34b6fb3f6381c7b6ea036
Zaher <ilbihz@gmail.com>
2021-10-17 12:49:34 -0400

    bare pseudocode for ex44

---
+++ Ex44/exercise44/src/main/java/baseline/JsonProcessor.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+//library to convert Java objects to JSON and vice-versa
+import com.google.gson.*;
+
+import java.io.File;
+
+
+public class JsonProcessor {
+
+
+    //method that will parse the information on the JSON file
+    public String parseJSONFile(){
+
+
+
+    }
+    //method that will search for a product name and retrieve its information
+    public void searchForItem(){
+
+    }
+
+
+
+
+
+
+}
---
+++ Ex44/exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Zaher Ilbih
+ */
+package baseline;
+
+
+
+public class Solution44 {
+
+
+    /*
+    Create a program that takes a product name as input and
+    retrieves the current price and quantity for that product.
+    The product data is in a data file in the JSON format  and
+    looks like this (you will create this file as `exercise44_input.json`):
+
+    {
+        "products" : [
+            {"name": "Widget", "price": 25.00, "quantity": 5 },
+            {"name": "Thing", "price": 15.00, "quantity": 5 },
+            {"name": "Doodad", "price": 5.00, "quantity": 10 }
+        ]
+    }
+
+    Print out the product name, price, and quantity if the product is found.
+    If no product matches the search, state that no product was found and start over.
+     */
+
+    //main method
+    public static void main(String[] args){
+
+        JsonProcessor sol = new JsonProcessor();
+
+
+    }
+
+}
